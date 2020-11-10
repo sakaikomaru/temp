ea5617fd669d71562f673be160d0664381c54adf
SakaiKomaru <1skikmr1@gmail.com>
2020-11-10 20:58:46 +0900

    commit

--- lib/BucketSort.hs
+++ lib/BucketSort.hs
@@ -9,6 +9,5 @@ import qualified Data.Vector.Unboxed as VU
 -- >>> bucketSort 100 (VU.fromList ([4, 8, 1, 6, 9, 3, 40, 13, 27] :: [Int]))
 -- [1,3,4,6,8,9,13,27,40]
 bucketSort :: Int -> VU.Vector Int -> VU.Vector Int
-bucketSort bucketSize
-  = VU.concatMap (uncurry $ flip VU.replicate) . VU.indexed . VU.unsafeAccumulate (+) (VU.replicate bucketSize 0) . VU.map (,1)
+bucketSort bucketSize = VU.concatMap (uncurry $ flip VU.replicate) . VU.indexed . VU.unsafeAccumulate (+) (VU.replicate bucketSize 0) . VU.map (,1)
 {-# INLINE bucketSort #-}
--- lib/SqrtMod.hs
+++ lib/SqrtMod.hs
@@ -1,15 +1,10 @@
 {-# LANGUAGE BangPatterns     #-}
-{-# LANGUAGE TypeApplications #-}
 
 module SqrtMod where
 
 import           Data.Bits
-import           Unsafe.Coerce
 import qualified GHC.Integer.GMP.Internals         as GMP
-import           Data.Time.Clock.POSIX             (getPOSIXTime)
 import qualified Data.Vector.Fusion.Stream.Monadic as VFSM
-import qualified Data.Vector.Unboxed               as VU
-import qualified Data.Vector.Unboxed.Mutable       as VUM
 
 sqrtTS :: Int -> Int -> IO Int
 sqrtTS a p
@@ -31,84 +26,6 @@ sqrtTS a p
 
 legendreSymbol :: Int -> Int -> Int
 legendreSymbol a p = powModInt a ((p - 1) `div` 2) p
-
-
-type RNG = VUM.IOVector Word
-
-getSeed :: IO Word
-getSeed = do
-  t <- getPOSIXTime
-  let ret = floor $ t * 1e9 :: Word
-  if ret == 0
-    then getSeed
-    else return ret
-
-splitMix64 :: Word -> (Word, Word)
-splitMix64 state = case state + 0x9e3779b97f4a7c15 of
-  z0 -> case (z0 .^. z0 .>>. 30) * 0xbf58476d1ce4e5b9 of
-    z1 -> case (z1 .^. z1 .>>. 27) * 0x94d049bb133111eb of
-      z2 -> case z2 .^. z2 .>>. 31 of
-        z3 -> (z0, z3)
-
-newRNG :: IO RNG
-newRNG = newRNGWithSeed =<< getSeed
-
-newRNGWithSeed :: Word -> IO RNG
-newRNGWithSeed seed = do
-  let (s0, seed') = splitMix64 seed
-  let (s1, _)     = splitMix64 seed'
-  rng <- VUM.unsafeNew 2
-  VUM.unsafeWrite rng 0 s0
-  VUM.unsafeWrite rng 1 s1
-  return rng
-
-nextWord :: RNG -> IO Word
-nextWord rng = do
-  !s0 <-            VUM.unsafeRead rng 0
-  !s1 <- xor s0 <$> VUM.unsafeRead rng 1
-  VUM.unsafeWrite rng 0 $ (s0 .<<. 24  .|.  s0 .>>. 40) .^. s1 .^. (s1 .<<. 16)
-  VUM.unsafeWrite rng 1 $ (s1 .<<. 37) .|. (s1 .>>. 27)
-  let s05 = s0 * 5
-  return $! (s05 .<<. 7 .|. s05 .>>. 57) * 9
-
-nextInt :: RNG -> IO Int
-nextInt rng = unsafeCoerce @Word @Int <$> nextWord rng
-
-nextDouble :: RNG -> IO Double
-nextDouble rng = do
-  t <- nextWord rng
-  let x = 0x3ff .<<. 52 .|. t .>>. 12
-  return $! unsafeCoerce @Word @Double x - 1.0
-
-nextGauss :: RNG -> Double -> Double -> IO Double
-nextGauss rng mu sigma = do
-  x <- nextDouble rng
-  y <- nextDouble rng
-  let z = sqrt (-2.0 * log x) * cos (2.0 * pi * y)
-  return $! sigma * z + mu
-
-withRNG :: (RNG -> IO a) -> IO a
-withRNG f = do
-  seed <- getSeed
-  withRNGWithSeed seed f
-
-withRNGWithSeed :: Word -> (RNG -> IO a) -> IO a
-withRNGWithSeed seed f = newRNGWithSeed seed >>= f
-
-shuffleM :: VUM.Unbox a => RNG -> VUM.IOVector a -> IO ()
-shuffleM rng mvec = do
-  rev (VUM.length mvec) $ \i -> do
-    j <- nextWord rng
-    VUM.unsafeSwap mvec i (unsafeCoerce $ rem j (unsafeCoerce i + 1))
-
-shuffle :: VU.Unbox a => VU.Vector a -> IO (VU.Vector a)
-shuffle vec = withRNG $ \rng -> do
-  mv <- VU.unsafeThaw vec
-  shuffleM rng mv
-  VU.unsafeFreeze mv
-
-randomR :: RNG -> (Int, Int) -> IO Int
-randomR rng (l, r) = flip mod (r - l + 1) <$> nextInt rng
 
 infixl 8 .>>., .<<.
 infixl 6 .^.
