17c5831783fca65d8286da367e7b2561a0ec1f3f
SakaiKomaru <1skikmr1@gmail.com>
2020-11-26 14:28:57 +0900

    commit

--- lib/LinearCongruentialGenerators.hs
+++ lib/LinearCongruentialGenerators.hs
@@ -1,9 +1,16 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE MagicHash    #-}
 
 module LinearCongruentialGenerators where
 
+import           Control.Monad.Cont
+import           Control.Monad.ST
+import           Data.Bits
+import           Data.Word
+import           GHC.Exts
 import           System.CPUTime
 import           Unsafe.Coerce
+import qualified GHC.Integer.GMP.Internals         as GMP
 import qualified Data.Vector.Fusion.Stream.Monadic as VFSM
 import qualified Data.Vector.Unboxed               as VU
 import qualified Data.Vector.Unboxed.Mutable       as VUM
@@ -21,14 +28,6 @@ _getNextRnd :: Integer -> Integer
 _getNextRnd n = (n * 307139 + 17) `mod` 891771648051262478234242325342735462117
 {-# INLINE _getNextRnd #-}
 
-fi :: Int -> Integer
-fi = fromIntegral
-{-# INLINE fi #-}
-
-fI :: Integer -> Int
-fI = fromInteger
-{-# INLINE fI #-}
-
 getNextRnd :: Int -> Int
 getNextRnd = flip mod (maxBound :: Int) . fI . _getNextRnd . fi
 {-# INLINE getNextRnd #-}
@@ -43,9 +42,7 @@ nextWord :: RNG -> IO Word
 nextWord = unsafeCoerce <$> nextInt
 
 randomR :: RNG -> Int -> Int -> IO Int
-randomR rng l r = do
-  x <- nextInt rng
-  return $ x `mod` (r - l + 1) + l
+randomR rng l r = (+ l) . flip mod (r - l + 1) <$> nextInt rng
 
 shuffleM :: VUM.Unbox a => RNG -> VUM.IOVector a -> IO ()
 shuffleM rng mvec = do
@@ -59,15 +56,167 @@ shuffle rng vec = do
   shuffleM rng mv
   VU.unsafeFreeze mv
 
-streamR :: Monad m => Int -> Int -> VFSM.Stream m Int
-streamR !l !r = VFSM.Stream step (r - 1)
-  where
-    step x
-      | x >= l    = return $ VFSM.Yield x (x - 1)
-      | otherwise = return VFSM.Done
-    {-# INLINE [0] step #-}
-{-# INLINE [1] streamR #-}
+-------------------------------------------------------------------------------
+-- for
+-------------------------------------------------------------------------------
+rep :: Monad m => Int -> (Int -> m ()) -> m ()
+rep n = flip VFSM.mapM_ (streamG 0 (n - 1) const 0 (+) 1)
+{-# INLINE rep #-}
+
+rep' :: Monad m => Int -> (Int -> m ()) -> m ()
+rep' n = flip VFSM.mapM_ (streamG 0 n const 0 (+) 1)
+{-# INLINE rep' #-}
+
+rep1 :: Monad m => Int -> (Int -> m ()) -> m ()
+rep1 n = flip VFSM.mapM_ (streamG 1 (n - 1) const 0 (+) 1)
+{-# INLINE rep1 #-}
+
+rep1' :: Monad m => Int -> (Int -> m ()) -> m ()
+rep1' n = flip VFSM.mapM_ (streamG 1 n const 0 (+) 1)
+{-# INLINE rep1' #-}
 
 rev :: Monad m => Int -> (Int -> m ()) -> m ()
-rev n = flip VFSM.mapM_ (streamR 0 n)
+rev n = flip VFSM.mapM_ (streamRG (n - 1) 0 const 0 (-) 1)
 {-# INLINE rev #-}
+
+rev' :: Monad m => Int -> (Int -> m ()) -> m ()
+rev' n = flip VFSM.mapM_ (streamRG n 0 const 0 (-) 1)
+{-# INLINE rev' #-}
+
+rev1 :: Monad m => Int -> (Int -> m ()) -> m ()
+rev1 n = flip VFSM.mapM_ (streamRG (n - 1) 1 const 0 (-) 1)
+{-# INLINE rev1 #-}
+
+rev1' :: Monad m => Int -> (Int -> m ()) -> m ()
+rev1' n = flip VFSM.mapM_ (streamRG n 1 const 0 (-) 1)
+{-# INLINE rev1' #-}
+
+range :: Monad m => Int -> Int -> (Int -> m ()) -> m ()
+range l r = flip VFSM.mapM_ (streamG l r const 0 (+) 1)
+{-# INLINE range #-}
+
+rangeR :: Monad m => Int -> Int -> (Int -> m ()) -> m ()
+rangeR r l = flip VFSM.mapM_ (streamRG r l const 0 (-) 1)
+{-# INLINE rangeR #-}
+
+forP :: Monad m => Int -> (Int -> m ()) -> m ()
+forP p = flip VFSM.mapM_ (streamG 2 p (^) 2 (+) 1)
+{-# INLINE forP #-}
+
+forG :: Monad m => Int -> Int -> (Int -> Int -> Int) -> Int -> (Int -> Int -> Int) -> Int -> (Int -> m ()) -> m ()
+forG l r f p g d = flip VFSM.mapM_ (streamG l r f p g d)
+{-# INLINE forG #-}
+
+forRG :: Monad m => Int -> Int -> (Int -> Int -> Int) -> Int -> (Int -> Int -> Int) -> Int -> (Int -> m ()) -> m ()
+forRG r l f p g d = flip VFSM.mapM_ (streamRG r l f p g d)
+{-# INLINE forRG #-}
+
+streamG :: Monad m => Int -> Int -> (Int -> Int -> Int) -> Int -> (Int -> Int -> Int) -> Int -> VFSM.Stream m Int
+streamG !l !r !f !p !g !d = VFSM.Stream step l
+  where
+    step x
+      | f x p <= r = return $ VFSM.Yield x (g x d)
+      | otherwise  = return VFSM.Done
+    {-# INLINE [0] step #-}
+{-# INLINE [1] streamG #-}
+
+streamRG :: Monad m => Int -> Int -> (Int -> Int -> Int) -> Int -> (Int -> Int -> Int) -> Int -> VFSM.Stream m Int
+streamRG !r !l !f !p !g !d = VFSM.Stream step r
+  where
+    step x
+      | f x p >= l = return $ VFSM.Yield x (g x d)
+      | otherwise  = return VFSM.Done
+    {-# INLINE [0] step #-}
+{-# INLINE [1] streamRG #-}
+
+withBreakIO :: ((r -> ContT r IO b) -> ContT r IO r) -> IO r
+withBreakIO = flip runContT pure . callCC
+{-# INLINE withBreakIO #-}
+
+withBreakST :: ((r -> ContT r (ST s) b) -> ContT r (ST s) r) -> (ST s) r
+withBreakST = flip runContT pure . callCC
+{-# INLINE withBreakST #-}
+
+-------------------------------------------------------------------------------
+-- util
+-------------------------------------------------------------------------------
+fi :: Int -> Integer
+fi = fromIntegral
+{-# INLINE fi #-}
+
+fI :: Integer -> Int
+fI = fromInteger
+{-# INLINE fI #-}
+
+powModInt :: Int -> Int -> Int -> Int
+powModInt a b c = fI $ GMP.powModInteger (fi a) (fi b) (fi c)
+{-# INLINE powModInt #-}
+
+recipModInt :: Int -> Int -> Int
+recipModInt a m = fI $ GMP.recipModInteger (fi a) (fi m)
+{-# INLINE recipModInt #-}
+
+infixl 8 .<<., .>>., .>>>.
+infixl 6 .^.
+
+(.<<.) :: Bits b => b -> Int -> b
+(.<<.) = unsafeShiftL
+{-# INLINE (.<<.) #-}
+
+(.>>.) :: Bits b => b -> Int -> b
+(.>>.) = unsafeShiftR
+{-# INLINE (.>>.) #-}
+
+(.>>>.) :: Int -> Int -> Int
+(.>>>.) (I# x#) (I# i#) = I# (uncheckedIShiftRL# x# i#)
+{-# INLINE (.>>>.) #-}
+
+(.^.) :: Bits b => b -> b -> b
+(.^.)  = xor
+{-# INLINE (.^.)  #-}
+
+clz :: FiniteBits fb => fb -> Int
+clz = countLeadingZeros
+{-# INLINE clz #-}
+
+ctz :: FiniteBits fb => fb -> Int
+ctz = countTrailingZeros
+{-# INLINE ctz #-}
+
+encode32x2 :: Int -> Int -> Int
+encode32x2 x y = x .<<. 32 .|. y
+{-# INLINE encode32x2 #-}
+
+decode32x2 :: Int -> (Int, Int)
+decode32x2 xy =
+    let !x = xy .>>>. 32
+        !y = xy .&. 0xffffffff
+    in (x, y)
+{-# INLINE decode32x2 #-}
+
+ceilPow2 :: Int -> Int
+ceilPow2 n
+  | n > 1     = (-1) .>>>. clz (n - 1) + 1
+  | otherwise = 1
+{-# INLINE ceilPow2 #-}
+
+floorPow2 :: Int -> Int
+floorPow2 n
+  | n >= 1    = 1 .<<. (63 - clz n)
+  | otherwise = 0
+{-# INLINE floorPow2 #-}
+
+bitReverse :: Int -> Int
+bitReverse
+  = unsafeCoerce
+  . step 32 0xffffffff00000000 0x00000000ffffffff
+  . step 16 0xffff0000ffff0000 0x0000ffff0000ffff
+  . step 08 0xff00ff00ff00ff00 0x00ff00ff00ff00ff
+  . step 04 0xf0f0f0f0f0f0f0f0 0x0f0f0f0f0f0f0f0f
+  . step 02 0xcccccccccccccccc 0x3333333333333333
+  . step 01 0xaaaaaaaaaaaaaaaa 0x5555555555555555
+  . unsafeCoerce
+  where
+    step :: Int -> Word64 -> Word64 -> Word64 -> Word64
+    step i ml mr = \ !x -> (x .&. ml) .>>. i .|. (x .&. mr) .<<. i
+    {-# INLINE step #-}
