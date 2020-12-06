0af2e45f1169a7814f8352908a9cc91ac2c737f6
sakaikomaru <72001395+sakaikomaru@users.noreply.github.com>
2020-12-06 10:23:04 +0900

    Update For.hs

--- lib/For.hs
+++ lib/For.hs
@@ -6,22 +6,26 @@ import           Control.Monad.Cont
 import           Control.Monad.ST
 import qualified Data.Vector.Fusion.Stream.Monadic as VFSM
 
+-------------------------------------------------------------------------------
+-- for
+-------------------------------------------------------------------------------
 {-
-rep    => [0 .. (n - 1)]
-rep'   => [0 .. n]
-rep1   => [1 .. (n - 1)]
-rep1'  => [1 .. n]
-rev    => [(n - 1) .. 0]
-rev'   => [n .. 0]
-rev1   => [(n - 1) .. 1]
-rev1'  => [n .. 1]
-range  => for (int i = l; i <= r; i++);
-rangeR => for (int i = r; i >= l; i--);
-forP   => for (int i = 2; i * i <= p; i++);
-forG   => for (int i = l; f(i, p) <= r; g(i,d));
-forRG  => for (int i = r; f(i, p) >= l; g(i,d));
+rep      => [0 .. (n - 1)]
+rep'     => [0 .. n]
+rep1     => [1 .. (n - 1)]
+rep1'    => [1 .. n]
+rev      => [(n - 1) .. 0]
+rev'     => [n .. 0]
+rev1     => [(n - 1) .. 1]
+rev1'    => [n .. 1]
+range    => for (int i = l; i <= r; i++);
+rangeR   => for (int i = r; i >= l; i--);
+forStep  => for (int i = l; i <= r; i += d);
+forStepR => for (int i = r; i >= l; i -= d);
+forP     => for (int i = 2; i * i <= p; i++);
+forG     => for (int i = l; f(i, p) <= r; g(i,d));
+forRG    => for (int i = r; f(i, p) >= l; g(i,d));
 -}
-
 rep :: Monad m => Int -> (Int -> m ()) -> m ()
 rep n = flip VFSM.mapM_ (stream 0 n)
 {-# INLINE rep #-}
@@ -62,6 +66,14 @@ rangeR :: Monad m => Int -> Int -> (Int -> m ()) -> m ()
 rangeR r l = flip VFSM.mapM_ (streamR l (r + 1))
 {-# INLINE rangeR #-}
 
+forStep :: Monad m => Int -> Int -> Int -> (Int -> m ()) -> m ()
+forStep l r d = flip VFSM.mapM_ (streamStep l r d)
+{-# INLINE forStep #-}
+
+forStepR :: Monad m => Int -> Int -> Int -> (Int -> m ()) -> m ()
+forStepR r l d = flip VFSM.mapM_ (streamStepR l r d)
+{-# INLINE forStepR #-}
+
 forP :: Monad m => Int -> (Int -> m ()) -> m ()
 forP p = flip VFSM.mapM_ (streamG 2 p (^) 2 (+) 1)
 {-# INLINE forP #-}
@@ -92,6 +104,24 @@ streamR !l !r = VFSM.Stream step (r - 1)
     {-# INLINE [0] step #-}
 {-# INLINE [1] streamR #-}
 
+streamStep :: Monad m => Int -> Int -> Int -> VFSM.Stream m Int
+streamStep !l !r !d = VFSM.Stream step l
+  where
+    step x
+      | x <= r    = return $ VFSM.Yield x (x + d)
+      | otherwise = return VFSM.Done
+    {-# INLINE [0] step #-}
+{-# INLINE [1] streamStep #-}
+
+streamStepR :: Monad m => Int -> Int -> Int -> VFSM.Stream m Int
+streamStepR !l !r !d = VFSM.Stream step r
+  where
+    step x
+      | x >= l    = return $ VFSM.Yield x (x - d)
+      | otherwise = return VFSM.Done
+    {-# INLINE [0] step #-}
+{-# INLINE [1] streamStepR #-}
+
 streamG :: Monad m => Int -> Int -> (Int -> Int -> Int) -> Int -> (Int -> Int -> Int) -> Int -> VFSM.Stream m Int
 streamG !l !r !f !p !g !d = VFSM.Stream step l
   where
@@ -117,3 +147,4 @@ withBreakIO = flip runContT pure . callCC
 withBreakST :: ((r -> ContT r (ST s) b) -> ContT r (ST s) r) -> (ST s) r
 withBreakST = flip runContT pure . callCC
 {-# INLINE withBreakST #-}
+
